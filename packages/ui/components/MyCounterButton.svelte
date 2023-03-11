c29ab77b21c566a48f9c51dd10b390497cc42e7b
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2023-03-11 17:25:12 +0700

    Initial commit

---
+++ packages/ui/components/MyCounterButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button class="primary" on:click="{handleClick}">
+  clicks count: {count}
+</button>
